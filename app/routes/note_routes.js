c703dc3097b9525af2ecf73df1d761877f95fb47
praveen399 <iampraveenpawar@gmail.com>
2018-03-13 13:18:42 +0530

    hi

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, db) {
-    app.post('/notes', (req, res) => {
-      // You'll create your note here.
-      res.send('Hello')
-    });
-  };
+  app.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.send('Hello')
+  });
+};
